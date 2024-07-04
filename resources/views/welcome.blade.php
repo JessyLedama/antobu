2dfa7f71f0d28fc4270ca04191e2fb17e5016d11
Jayliste <jaylisteledama94@gmail.com>
2024-07-04 09:26:50 +0300

    [UPDATE] updated add-to-cart button in product card in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,36 +59,41 @@
             <div class="container">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
                     @foreach($products as $product)
-                    <a href="{{ route('product.show', $product->slug) }}">
                         <div class="col">
                             <div class="card shadow-sm">
-                                <img src="{{ asset('/storage/'.$product->image) }}" alt="" width="100%" height="225">
+                                <a href="{{ route('product.show', $product->slug) }}">  
+                                    <img src="{{ asset('/storage/'.$product->image) }}" alt="" width="100%" height="225">
 
+                                    <div class="card-body">
+
+                                        <h3 class="card-text">
+                                            {{ ucwords($product->name) }}
+                                        </h3>
+
+                                        <p class="card-text">
+                                            {{ ucfirst($product->description) }}
+                                        </p>
+                                    </div>
+
+                                </a>
+                                        
                                 <div class="card-body">
-
-                                    <h3 class="card-text">
-                                        {{ ucwords($product->name) }}
-                                    </h3>
-
-                                    <!-- <p class="card-text">
-                                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                                    </p> -->
-                                    
                                     <div class="d-flex justify-content-between align-items-center">
                                         <small class="text-body-secondary">
                                             KSh. {{ $product->price }}
                                         </small>
 
                                         <div class="btn-group">
-                                            <button type="button" class="btn btn-sm btn-outline-secondary">
+                                            <a href="{{ route('addToCart', $product->slug) }}" type="button" class="btn btn-sm btn-outline-secondary">
+                                                <span class="fa fa-cart-plus"></span>
                                                 Add to Cart
-                                            </button>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
+                                
                             </div>
                         </div>
-                    </a>
                     @endforeach
                 </div>
             </div>
